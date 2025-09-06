ac42e14bb9b94d4e795ebacfa96f01af39e748c5
Aniyah C. <aniyahcurry12@gmail.com>
2025-09-06 18:23:31 -0500

    small styling changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
-        <p className="phrase">Jadosi!</p>
-        <p className="attribute">Built by Aniyah Curry</p>
+    <div>
+        {/*<p className="phrase">Jadosi!</p>*/}
+        <p className="attribute">Built by Aniyah Curry, 2025 💫</p>
     </div>
   );
 };
